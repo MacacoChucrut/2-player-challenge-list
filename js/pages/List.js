0f0e1b4bf6118b46e999caf48c8fc3baae133eac
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2025-09-18 20:25:47 -0300

    Add files via upload

---
+++ js/pages/List.js
@@ -0,0 +1,197 @@
+import { store } from "../main.js";
+import { embed } from "../util.js";
+import { score } from "../score.js";
+import { fetchEditors, fetchList } from "../content.js";
+
+import Spinner from "../components/Spinner.js";
+import LevelAuthors from "../components/List/LevelAuthors.js";
+
+const roleIconMap = {
+    owner: "crown",
+    admin: "user-gear",
+    helper: "user-shield",
+    dev: "code",
+    trial: "user-lock",
+};
+
+export default {
+    components: { Spinner, LevelAuthors },
+    template: `
+        <main v-if="loading">
+            <Spinner></Spinner>
+        </main>
+        <main v-else class="page-list">
+            <div class="list-container">
+                <table class="list" v-if="list">
+                    <tr v-for="([level, err], i) in list">
+                        <td class="rank">
+                        <p class="type-label-lg" :style="{ color: i + 1 > 50 ? 'darkgrey' : 'inherit' }"> #{{ i + 1 }}</p>
+                        </td>
+                        <td class="level" :class="{ 'active': selected == i, 'error': !level }">
+                            <button @click="selected = i">
+                                <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
+                            </button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="level-container">
+                <div class="level" v-if="level">
+                    <h1>{{ level.name }}</h1>
+                    <LevelAuthors :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
+                    <div style="display:flex">
+                        <div v-for="tag in level.tags" class="tag">{{tag}}</div>
+                    </div>
+                    <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
+                    <ul class="stats">
+                        <li>
+                            <div class="type-title-sm">Points when completed</div>
+                            <p>{{ score(selected + 1, 100, level.percentToQualify) }}</p>
+                        </li>
+                        <li>
+                            <div class="type-title-sm">ID</div>
+                            <p>{{ level.id }}</p>
+                        </li>
+                    </ul>
+                    <h2>Victors</h2>
+                    <p v-if="selected + 1 > 50">This level has fallen into the Legacy List and no longer accepts new records.</p>
+                    <table class="records">
+                        <tr v-for="record in level.records" class="record">
+                            <td class="percent">
+                                <p>{{ record.percent }}%</p>
+                            </td>
+                            <td class="user">
+                                <a :href="record.link" target="_blank" class="type-label-lg">{{ record.user }}</a>
+                            </td>
+                            <td class="mobile">
+                                <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
+                    <p>(ノಠ益ಠ)ノ彡┻━┻</p>
+                </div>
+            </div>
+            <div class="meta-container">
+                <div class="meta">
+                    <div class="errors" v-show="errors.length > 0">
+                        <p class="error" v-for="error of errors">{{ error }}</p>
+                    </div>
+                    <div class="og">
+                        <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
+                    </div>
+                    <template v-if="editors">
+                        <h3>List Editors</h3>
+                        <ol class="editors">
+                            <li v-for="editor in editors">
+                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
+                                <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
+                                <p v-else>{{ editor.name }}</p>
+                            </li>
+                        </ol>
+                    </template>
+                    <h3>Level Rules</h3>
+                    <p>
+                        The level has to be under 30 seconds when played. Using speed portals to make the game show the time as under 30 seconds is not allowed.
+                    </p>
+                    <p>
+                        For a level to place, it must be harder than the level placed at #75.
+                    </p>
+                    <p>
+                        No minigames. The level should play like Geometry Dash.
+                    </p>
+                    <p>
+                        Anything using the random trigger must not notably affect the gameplay or visual difficulty.
+                    </p>
+                    <p>
+                        If the challenge copies parts from a full level, it must be sufficiently different from the original. If the original level was already a 2 Player level, this rule will be enforced more strictly.
+                    </p>
+                        <h3>Submission Requirements</h3>
+                    <p>
+                        All records must be achieved solo.
+                    </p>
+                    <p>
+                        Do not bind one key to both inputs (meaning you can't have one key that jumps with player 1 and player 2).
+                    </p>
+                    <p>
+                        Do not use more than 4 keys bound to inputs in a completion. For example, you can have two keys bound to player 1 and two keys bound to player 2.
+                    </p>
+                    <p>
+                        Do not use major secret routes or bug routes. If you are unsure if a skip is invalid, contact an admin.
+                    </p>
+                    <p>
+                        Achieved the record on the level that is listed on the site. Please check the level ID before you submit a record.
+                    </p>
+                    <p>
+                        Have clicks/taps in the video. Handcam will only be needed for levels that have the requirement for it.
+                    </p>
+                    <p>
+                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt.
+                    </p>
+                    <p>
+                        The recording must show the level complete screen, or the completion will be invalidated.
+                    </p>
+                    <p>
+                        Once a level falls onto the Legacy List (#51 and below), we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level.
+                    </p>
+                </div>
+            </div>
+        </main>
+    `,
+    
+    data: () => ({
+        list: [],
+        editors: [],
+        loading: true,
+        selected: 0,
+        errors: [],
+        roleIconMap,
+        store
+    }),
+    computed: {
+        level() {
+            return this.list[this.selected][0];
+        },
+        video() {
+            if (!this.level.showcase) {
+                return embed(this.level.verification);
+            }
+
+            return embed(
+                this.toggledShowcase
+                    ? this.level.showcase
+                    : this.level.verification
+            );
+        },
+    },
+    async mounted() {
+        // Hide loading spinner
+        this.list = await fetchList();
+        this.editors = await fetchEditors();
+
+        // Error handling
+        if (!this.list) {
+            this.errors = [
+                "Failed to load list. Retry in a few minutes or notify list staff.",
+            ];
+        } else {
+            this.errors.push(
+                ...this.list
+                    .filter(([_, err]) => err)
+                    .map(([_, err]) => {
+                        return `Failed to load level. (${err}.json)`;
+                    })
+            );
+            if (!this.editors) {
+                this.errors.push("Failed to load list editors.");
+            }
+        }
+
+        this.loading = false;
+    },
+    methods: {
+        embed,
+        score,
+    },
+};
